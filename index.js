ba4e773986d16223dc329c0c76c3d80b0cba6bef
Yuki <yuki_matsubara@outlook.com>
2021-04-13 01:15:40 -0700

    about updated

--- index.js
+++ index.js
@@ -9,14 +9,23 @@ const aboutSection = document.querySelector(".about");
 const skillSection = document.querySelector(".skills");
 const workSection = document.querySelector(".work");
 const reachMe = document.querySelector(".contact");
+//About section buttons
+const careerBtn = document.querySelector(".careerBtn");
+const educationBtn = document.querySelector(".educationBtn");
+//modal
+const career = document.querySelector(".career");
+const education = document.querySelector(".education");
+const modal = document.querySelector(".modal");
+const clsCar = document.querySelector(".clsCar");
+const clsEdu = document.querySelector(".clsEdu");
 
 /* Function Declaration */
 const smoothScroll = (element) => {
   element.scrollIntoView({ behavior: "smooth" });
 }
 
-
 /* Function Call */
+//scroll
 aboutBtn.addEventListener("click", () => {
   smoothScroll(aboutSection);
 });
@@ -32,3 +41,43 @@ workBtn.addEventListener("click", () => {
 contactBtn.addEventListener("click", () => {
   smoothScroll(reachMe);
 });
+
+//modal
+careerBtn.addEventListener("click", () => {
+  career.style.display = "block";
+});
+
+educationBtn.addEventListener("click", () => {
+  education.style.display = "block";
+});
+
+clsCar.addEventListener("click", () => {
+  career.style.display = "none";
+});
+
+clsEdu.addEventListener("click", () => {
+  education.style.display = "none";
+});
+
+// window.onclick = function (event) {
+//   console.log("career ",event.target);
+//   if(event.target === career) {
+//     education.style.display = "none";
+//     career.style.display = "none";
+//   }
+// };
+
+// window.onclick = function (event) {
+//   console.log("education ",event.target);
+//   if(event.target === education) {
+//     education.style.display = "none";
+//     career.style.display = "none";
+//   }
+// };
+
+// window.onclick = function (event) {
+//   console.log("target ",event.target);
+//   if((event.target === career) ||(event.target === education) )
+//     career.style.display = "none";
+//     education.style.display = "none";
+// };
